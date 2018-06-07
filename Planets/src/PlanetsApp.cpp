8c7089a8826fc343ae9dc090c91522d901eeab9f
Joe Bentley <joebentley10@gmail.com>
2018-06-07 22:20:40 +0100

    Added planetary rotation

--- Planets/src/PlanetsApp.cpp
+++ Planets/src/PlanetsApp.cpp
@@ -14,25 +14,38 @@ public:
     Planet &setRadius(float radius) { mRadius = radius; return *this; }
     Planet &setOrbitingRadius(float radius) { mOrbitingRadius = radius; return *this; }
     Planet &setOrbiting(Planet *orbiting) { mOrbiting = orbiting; return *this; }
-    Planet &setOrbitingAngle(float angle) { mAngle = angle; return *this; }
-    Planet &addOrbitingAngle(float angle) { mAngle += angle; return *this; }
     Planet &setTexture(gl::Texture2dRef tex) { mTex = tex; return *this; }
     
+    Planet &setOrbitingAngleFrequency(float omega) { mOrbitingAngleFrequency = omega; return *this; }
+    Planet &setAngularFrequency(float omega) { mAngularFreq = omega; return *this; }
+    
+    void animate(float timeInSeconds);
+    
     mat4 getModelMatrix();
+    mat4 getModelMatrixWithScaling() {
+        return getModelMatrix() * glm::scale(vec3(mRadius));
+    }
+    
     void draw(vec3 lightCoord);
     void draw();
 private:
     gl::BatchRef mBatch;
     gl::Texture2dRef mTex {nullptr};
     mat4 mOrbitingPos;
-    mat4 mModel;
     Planet *mOrbiting {nullptr};
     
     float mRadius {1};
     float mOrbitingRadius {3};
+    float mOrbitingAngle {0};
+    float mOrbitingAngleFrequency {0};
     float mAngle {0};
+    float mAngularFreq {0};
     
     void maybeBindTexture();
+    
+    // Cache model matrix
+    bool mDirtyModelMatrix {true};
+    mat4 mModelMatrix;
 };
 
 void Planet::maybeBindTexture() {
@@ -43,21 +56,26 @@ void Planet::maybeBindTexture() {
 }
 
 mat4 Planet::getModelMatrix() {
+    if (!mDirtyModelMatrix)
+        return mModelMatrix;
+    
     mat4 modelMatrix;
     
     if (mOrbiting != nullptr)
         modelMatrix *= mOrbiting->getModelMatrix();
-    modelMatrix *= glm::rotate(mAngle, vec3(0, 1, 0));
+    modelMatrix *= glm::rotate(mOrbitingAngle, vec3(0, 1, 0));
     modelMatrix *= glm::translate(vec3(mOrbitingRadius, 0, 0));
-    modelMatrix *= glm::scale(vec3(mRadius));
+    modelMatrix *= glm::rotate(mAngle, vec3(0, 1, 0));
     
-    return modelMatrix;
+    mDirtyModelMatrix = false;
+    mModelMatrix = modelMatrix;
+    return mModelMatrix;
 }
 
 void Planet::draw(vec3 lightCoord) {
     gl::ScopedModelMatrix scpMtx;
-    gl::multModelMatrix(getModelMatrix());
-    auto invModelMatrix = glm::inverse(gl::getModelMatrix());
+    gl::multModelMatrix(getModelMatrixWithScaling());
+    auto invModelMatrix = glm::inverse(getModelMatrix());
     mBatch->getGlslProg()->uniform("uLightCoord",
                                     glm::normalize(vec3(invModelMatrix * vec4(lightCoord, 1))));
     
@@ -67,9 +85,15 @@ void Planet::draw(vec3 lightCoord) {
 
 void Planet::draw() {
     gl::ScopedModelMatrix scpMtx;
-    gl::multModelMatrix(getModelMatrix());
+    gl::multModelMatrix(getModelMatrixWithScaling());
     maybeBindTexture();
     mBatch->draw();
+}
+
+void Planet::animate(float timeInSeconds) {
+    mOrbitingAngle = mOrbitingAngleFrequency * timeInSeconds;
+    mAngle = mAngularFreq * timeInSeconds;
+    mDirtyModelMatrix = true;
 }
 
 class PlanetsApp : public App {
@@ -83,7 +107,7 @@ private:
 //    gl::BatchRef mSun;
     
     CameraPersp mCam;
-    vec3 cInitialEyeLocation {4.0f * vec3(4, 3, 4)};
+    vec3 cInitialEyeLocation {3.0f * vec3(4, 3, 4)};
     const float cRotationPerFrame;
     quat mRotation {0, vec3(0, 1, 0)};
     
@@ -110,13 +134,18 @@ void PlanetsApp::setup()
     auto sunBatch = gl::Batch::create(geomSphere, glslNoLight);
     
     mSun = std::make_unique<Planet>(sunBatch);
-    mSun->setOrbitingRadius(0)
+    mSun->setOrbitingRadius(0).setRadius(1.4f)
+        .setAngularFrequency(2 * M_PI * 0.01f)
         .setTexture(gl::Texture2d::create(loadImage(loadAsset("textures/sun.jpg"))));
     mEarth = std::make_unique<Planet>(planetBatch);
     mEarth->setOrbiting(mSun.get()).setOrbitingRadius(5)
+        .setOrbitingAngleFrequency(2 * M_PI * 0.1f)
+        .setAngularFrequency(2 * M_PI * 0.05f)
         .setTexture(gl::Texture2d::create(loadImage(loadAsset("textures/earth.jpg"))));;
     mMoon = std::make_unique<Planet>(planetBatch);
     mMoon->setOrbiting(mEarth.get()).setOrbitingRadius(2).setRadius(0.4)
+        .setOrbitingAngleFrequency(2 * M_PI * 0.1f)
+        .setAngularFrequency(2 * M_PI * 0.1f)
         .setTexture(gl::Texture2d::create(loadImage(loadAsset("textures/moon.jpg"))));;
     
     gl::enableDepth();
@@ -131,14 +160,14 @@ void PlanetsApp::draw()
     mCam.lookAt(eyeLocation, vec3(0));
     gl::setMatrices(mCam);
     
-    auto angle = static_cast<float>(0.5 * M_PI * getElapsedSeconds());
     gl::color(0.8, 0.8, 0.8);
     
     mEarth->draw(mLightCoord);
-    mEarth->setOrbitingAngle(angle);
+    mEarth->animate(getElapsedSeconds());
     mMoon->draw(mLightCoord);
-    mMoon->setOrbitingAngle(2 * angle);
+    mMoon->animate(getElapsedSeconds());
     mSun->draw();
+    mSun->animate(getElapsedSeconds());
 }
 
 void PlanetsApp::mouseDown(MouseEvent event)
