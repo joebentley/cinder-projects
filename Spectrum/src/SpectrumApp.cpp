7359bab8df4268de5963908360cbe8e1f26cee04
Joe Bentley <joebentley10@gmail.com>
2018-06-09 18:46:50 +0100

    Adding camera zoom on beat

--- Spectrum/src/SpectrumApp.cpp
+++ Spectrum/src/SpectrumApp.cpp
@@ -25,12 +25,17 @@ private:
     gl::BatchRef mSegment;
     CameraPersp mCam;
     audio::MonitorSpectralNodeRef mSpectral;
+    audio::MonitorNodeRef mMonitor;
     
     const float cGraphPower {0.7/*1*/};
     const int cNumFftBins {60};
     const float cBarWidth {0.04f};
     const float cBarSpacing {0.01f};
-    float mMaxMagnitudeSoFar {0};
+    const float cMinimumBarHeight {0.04f};
+    float mMaxMagnitudeSoFar {0.01f};
+    
+    const float cInitialRotationDelay {0.5f};
+    const float cCameraZoomAmount {0.3f};
     
     const vec3 cInitialCamPos {0, 1.7, 4};
     const vec3 cLookingAtPos {0, 0.2, 0};
@@ -39,6 +44,9 @@ private:
     std::deque<std::vector<float>> mMovingAverageValues;
     
     float calculateMovingAverageValue(int i) const;
+    
+    const size_t cVolumeMovAvgValues {5};
+    std::deque<float> mVolumeMovAvgValues;
     
     gl::GlslProgRef mGlsl;
     vec3 mLightCoord {1.5, 1, -1};
@@ -74,6 +82,9 @@ void SpectrumApp::setup()
     mSpectral = ctx->makeNode<audio::MonitorSpectralNode>(audio::MonitorSpectralNode::Format()
                                                           .fftSize(cNumFftBins).windowSize(1024));
     input >> mSpectral;
+    
+    mMonitor = ctx->makeNode<audio::MonitorNode>();
+    input >> mMonitor;
 }
 
 void SpectrumApp::draw()
@@ -81,19 +92,40 @@ void SpectrumApp::draw()
     gl::clear( Color( 0, 0, 0 ) );
     gl::setMatrices(mCam);
     
-    const float anim = fmod(0.3f * getElapsedSeconds(), 2.0f);
+    const float anim = fmod(0.3f * getElapsedSeconds() - cInitialRotationDelay, 2.0f);
     
     const auto angle = static_cast<float>(M_PI * (easeInOutAtan(fmod(anim, 1.0f), 5.5f) + (anim > 1.0f ? 1.0f : 0.0f)));
-    mCam.lookAt(vec3(glm::rotate(angle, vec3(0, 1, 0)) * vec4(cInitialCamPos, 1)), cLookingAtPos);
+    auto eyePosition = vec3(glm::rotate(angle, vec3(0, 1, 0)) * vec4(cInitialCamPos, 1));
+    
+    const auto volume = mMonitor->getVolume();
+    mVolumeMovAvgValues.push_front(volume);
+    if (mVolumeMovAvgValues.size() > cVolumeMovAvgValues)
+        mVolumeMovAvgValues.pop_back();
+    
+    float averageVolume = 0;
+    for (auto f : mVolumeMovAvgValues)
+        averageVolume += f;
+    
+    averageVolume /= cVolumeMovAvgValues;
+    
+    const auto zoom = 1 - cCameraZoomAmount * averageVolume;
+    eyePosition = zoom * eyePosition;
+    
+    mCam.lookAt(eyePosition, cLookingAtPos);
     
     mGlsl->uniform("uLightCoord", glm::normalize(mLightCoord));
     
 #ifdef DEBUG___
-    {
-        gl::ScopedModelMatrix scpMtx;
-        gl::translate(mLightCoord);
-        mDebugLight->draw();
-    }
+    gl::pushMatrices();
+    gl::translate(mLightCoord);
+    mDebugLight->draw();
+    gl::popMatrices();
+    
+    gl::pushMatrices();
+    const auto vol = 2 * averageVolume;
+    gl::translate(vec3(0.5, vol, 0.5));
+    mDebugLight->draw();
+    gl::popMatrices();
 #endif
     
     const auto &magSpectrum = mSpectral->getMagSpectrum();
@@ -107,7 +139,8 @@ void SpectrumApp::draw()
         if (mag > mMaxMagnitudeSoFar)
             mMaxMagnitudeSoFar = mag;
         
-        const auto magNormalized = clamp(pow(mag / mMaxMagnitudeSoFar, cGraphPower), 0.05f, 1.0f);
+        const auto magNormalized = cMinimumBarHeight
+                                 + (1 - cMinimumBarHeight) * pow(mag / mMaxMagnitudeSoFar, cGraphPower);
         
         gl::ScopedModelMatrix scpMtx;
         gl::ScopedColor scpCol;
